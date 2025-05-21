d0f17b83bcecfc804a250292f5df4d198dccd3e6
edudelsol <edu_epsilon@hotmail.com>
2025-05-21 16:12:59 +0100

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended', eslintConfigPrettier],
+    rules: {
+      semi: ['error', 'always'],
+      'no-var': 'error',
+      indent: ['warn', 2],
+      'no-empty': ['warn', { allowEmptyCatch: true }],
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ],
+      'no-unused-private-class-members': 'warn'
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: globals.browser
+    }
+  },
+  {
+    files: ['webpack.*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node
+    },
+    rules: {
+      'no-var-requires': 'off'
+    }
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['package-lock.json'],
+    rules: {
+      'json/no-empty-keys': 'off'
+    }
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended']
+  }
+]);
---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+// webpack.common.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      minify: {
+        removeRedundantAttributes: false
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/style.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/resource',
+        resourceQuery: { not: [/inline/] }
+      },
+      {
+        test: /\.svg$/i,
+        use: 'raw-loader',
+        resourceQuery: /inline/
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+};
